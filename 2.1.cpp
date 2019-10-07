a79f973a7f40a048431540dc7d14ac14bbf813cf
riskybandar <56248339+riskybandar@users.noreply.github.com>
2019-10-07 17:01:45 +0700

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+void main ()
+{
+	int t;
+	int *kt;
+	t=8;
+	kt = &t;
+	cout<<"Nilai t				: "<<t<<endl;
+	cout<<"Nilai *kt			: "<<t<<endl;
+	cout<<"Nilai kt (alamat k)	: "<<t<<endl;
+	getch();
+}
